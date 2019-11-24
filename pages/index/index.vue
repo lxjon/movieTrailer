2975211261d260b2796be1090af05a120c06e873
lerojon <leroy@qq.com>
2019-11-24 22:36:14 +0800

    swiper

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="">
+		<swiper :indicator-dots="true" :autoplay="true" class="carousel" >
+			<swiper-item>
+				<image src="../../static/carousel/batmanvssuperman.png" class="carousel"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="../../static/carousel/spiderman.png" class="carousel"></image>
+			</swiper-item>
+			<swiper-item>
+				
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
